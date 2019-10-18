57ede86d70655294a6256839ee71014e090dfc9e
ashleyprovolt <55499074+ashleyprovolt@users.noreply.github.com>
2019-10-18 09:59:36 -0600

    Add files via upload

--- project_two.sql
+++ project_two.sql
@@ -25,10 +25,16 @@ create table borrower (
 	borrower_LName varchar(100) not null
 );
 
+create table artist_type	(
+	 artist_type_ID int not null primary key,
+	 artist_description varchar(100) not null
+);
+
 create table artist (
 	artist_ID int not null identity primary key,
+	artist_type_ID int not null references artist_type(artist_type_ID),
 	artist_FName varchar(100) not null,
-	artist_LName varchar(100) null,
+	artist_LName varchar(100) null
 );
 
 create table genre (
@@ -44,6 +50,7 @@ create table status (
 create table CD (
 	CD_ID int not null identity primary key,
 	CD_Name varchar(100) not null,
+	release_Date smalldatetime not null,
 	genre_ID int not null references genre(genre_ID),
 	status_ID int not null references status(status_ID)
 );
@@ -54,6 +61,8 @@ create table CD_artist (
 	primary key(artist_ID, CD_ID) 	
 );
 
+--drop table borrower_CD;
+
 create table borrower_CD (
 	borrower_ID int not null references borrower(borrower_ID),
 	CD_ID       int not null references CD(CD_ID),
@@ -61,21 +70,6 @@ create table borrower_CD (
 	returned_date smalldatetime null
 	primary key(borrower_ID, CD_ID, borrower_date)
 );
---create index PK_borrower_CD on borrower_CD(borrower_ID, CD_ID, borrower_date);
-
---create table diskhasBorrower (
---	borrower_ID int not null references borrower(borrower_ID),
---	disk_ID     int not null, --refernces disk(disk_id)
---	borrower_date smalldatetime not null,
---	returned_date smalldatetime null
---);
-
---create table diskHasArtist (
---	disk_id  int not null, --refernce disk(disk_id)
---	artist_ID int not null references artist(artist_ID)
---);
---create index PK_diskHasArtist on diskHasArtist(disk_id, artist_ID)
-
 
 
 
